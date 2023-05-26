de1ce5a47895061013281da30089d168e4fcef22
Dilara <dcavus@gmail.com>
2023-05-25 20:05:52 -0700

    committing docker files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+#creates a base image  where the other images are going to buiilt on
+#FROM specifies the base image from which you'll build your new image
+FROM amd64/maven:3.8.6-openjdk-11
+# sets the working ddirectory for subsequent instructions
+WORKDIR usr/app
+#copies files and directories from the local file system to the image
+COPY  .  .
+#Run the jar file with Entrypoint
+#specifies the default the executable container
+ENTRYPOINT ["mvn","spring-boot:run"]
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,13 +12,13 @@ VALUES ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Alice', 'Jon
        ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'David', 'Lee', 'davidlee@na.edu','davidlee@na.edu',  '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK','103',3),
        ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Emily', 'Taylor', 'emilytaylor@na.edu', 'emilytaylor@na.edu','$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK','104',3);
 
-INSERT INTO devices(insert_date_time, insert_user_id, is_deleted, last_update_date_time,
-                    last_update_user_id,make, model, serial_number, price, quantity, check_me_out)
-VALUES('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Samsung', 'Galaxy S21', 123456789012345, 89900, 10, true),
-      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Apple', 'iPhone 12 Pro', 234567890123456, 109900, 5, true),
-      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Google', 'Pixel 5', 345678901234567, 79900, 20, true),
-      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'OnePlus', '8T', 456789012345678, 59900, 15, true),
-      ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Motorola', 'Moto G Power', 567890123456789, 24900, 25, true);
+-- INSERT INTO devices(insert_date_time, insert_user_id, is_deleted, last_update_date_time,
+--                     last_update_user_id,make, model, serial_number, price, quantity, check_me_out)
+-- VALUES('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Samsung', 'Galaxy S21', 123456789012345, 89900, 10, true),
+--       ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Apple', 'iPhone 12 Pro', 234567890123456, 109900, 5, true),
+--       ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Google', 'Pixel 5', 345678901234567, 79900, 20, true),
+--       ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'OnePlus', '8T', 456789012345678, 59900, 15, true),
+--       ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Motorola', 'Moto G Power', 567890123456789, 24900, 25, true);
 
 insert into transactions(insert_date_time, insert_user_id, is_deleted, last_update_date_time,
                          last_update_user_id,description) values ('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,'Assign'),('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1, 'Retrieve');
